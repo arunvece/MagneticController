203daab24d2b755d48e0aa6646903683accc93ba
elquimico <henrycouncil@gmail.com>
2015-10-16 17:13:32 -0500

    updated model code

---
+++ Code/MainLoop.m
@@ -0,0 +1,19 @@
+%Main (fast) loop for control of sphere
+
+format compact
+close all
+clear all
+clc
+
+%limit = 0.8;
+%s = 0;
+
+while 1
+    tmp = rand;
+    if tmp > limit %Break loop condition
+        break
+    end
+    %s = s + tmp;
+    
+    
+end
