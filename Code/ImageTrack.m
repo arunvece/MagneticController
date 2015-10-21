5f7cb40d411a969908149b4b0f39a60ee8299dc8
elquimico <henry@unknown3402865757a6.attlocal.net>
2015-10-20 19:15:17 -0500

    Code Update and Weekly Reports uploaded

---
+++ Code/ImageTrack.m
@@ -0,0 +1,142 @@
+%ways to speed it up:
+% Add a ROI --
+% change parameters on imfindcircles
+% find a faster detector than imfindcircles  (using a ROI, a very simple
+% blob detector, tuned to find circles of a given size, may work)
+% I'm not finding the end of the video correctly
+
+addpath('../Test Videos/')
+format compact
+close all
+clear all
+clc
+%videoName = 'output_new.avi' %'output3.mp4'% 'output3.ogg';
+%display(['trying to load ', videoName])
+%vidBead = VideoReader(videoName);
+%numframes = vidBead.NumberOfFrames;
+frame = 1;
+
+minPulse = 1000e-6;
+maxPulse = 2000e-6;
+
+obj = imaq.VideoDevice('dcam',1,'F7_Y8_640x480_mode0'); %camera link and object 
+display('Connecting to Arduino');
+a=arduino('/dev/ttyS101','Mega2560'); %create object for controller
+%controller; not needed for dummy testing
+s = servo(a, 'D4', 'MinPulseDuration', minPulse, 'MaxPulseDuration', maxPulse); %connect PWN control wire to digital pin #4.
+
+numframes = 399;
+xyCMD = [400,400];
+%rect = [0 0 480 864];
+%yCMD = 300;
+I_MAX = 20;
+%I_CMD = 0;
+%I_OUT = 0;
+I_CMD_x = 0;
+I_CMD_y = 0;
+error_gain = [0,0];
+
+ballpos = zeros(numframes,2);
+error_xy = [0,0];
+%f = figure(1);
+I=step(obj);
+%I = step(vidBead);
+%h = imshow(I);
+%e = imellipse(gca, [333 208 96 89]);
+%ballCMDpos = [xCMD,yCMD];
+%B2 = createMask(e,h_im);
+
+%rROI = 25; %radius of ROI
+sx = 400;
+sy = 400;
+
+% Ball is being tracked by colour intensity. Light colours are filtered out, and avareage of X and Y coordinates of the rest makes the output. Only region covered by blue square (generated with X and Y coordinates from previous frame) is taken into calculations for better performance
+
+for frame = 1:numframes
+    try
+       I=step(obj);
+       %I = im2double(read(vidBead,frame)); %read frame
+    catch e
+        display (e);
+    end
+   if (frame>5)
+            %e = imellipse(gca, [sx sy 50 50]);
+            %h_im = imshow(I);
+            %h = imrect(gca, [(sx-50) (sy-50) 100 100]);
+            %mask = createMask(h,h_im);
+            Ymin = max(100,round(sy-50));
+            Ymax = max(400,round(sy + 200));
+            Xmin = max(100,round(sx-50));
+            Xmax = max(400,round(sx + 200));
+            level = graythresh(I);
+            BW = im2bw(I,0.7);
+            %imshow(BW((Ymin:Ymax),(Xmin:Xmax)));
+            %BW2 = BW((Ymin:Ymax),(Xmin:Xmax));
+            %I2 = imcrop(I,h);
+            %level = graythresh(I2);
+            %BW = im2bw(I2,.7);
+            %imshow(I);  
+            
+    else
+            level = graythresh(I);
+            BW = im2bw(I,.7);
+            Xmin = 1;
+            Xmax = 640;
+            Ymin = 1;
+            Ymax = 480;
+            %imshow(BW((Ymin:Ymax),(Xmin:Xmax)));
+            
+    end 
+        
+        
+        %d = imdistline;  %Run this to find object's diameter is about 20.
+        %Divide by two to get radius
+        %12.8 7 to 13
+        %12.8 7 to 13
+        
+        % thanks: http://www.mathworks.com/help/images/examples/detect-and-measure-circular-objects-in-an-image.html?refresh=true
+        %[centers, radii] = imfindcircles(I,[7
+        %10],'ObjectPolarity','dark','Sensitivity',0.95); Camera Feed only
+        [centers, radii] = imfindcircles(BW((Ymin:Ymax),(Xmin:Xmax)),[7 15],'ObjectPolarity','dark','Sensitivity',0.93);
+        %Motor Controller
+        if numel(centers)>1
+            %Correctedcenters = [(centers(1,1)+
+            error_xy = xyCMD - centers(1,1:2);  %find error in position
+            sx = centers(1,1);
+            sy = centers(1,2);
+            %rect = [(sx-rROI) (sy-rROI) (sx+rROI) (sy+rROI)]; 
+        else
+            display('using old data')
+        end
+        %sx = centers(1,1);
+        %sy = centers(1,2);
+        
+        error_gain = Controller(error_xy);
+        
+        I_CMD_x = error_gain(1,1);
+        I_CMD_y = error_gain(1,2);
+        
+        RealArduino(I_CMD_x,I_CMD_y,s);
+           
+     ballpos(frame,:) = centers(1,1:2);
+
+     %display e;
+     %display "Error found";
+     %end
+     %imshow(I2);
+     %set(h,'CData',I);
+     %h = viscircles(centers,radii); %need to scale?
+     %title(['Frame ', num2str(frame), ', Centroid: [',num2str(centers(1,1)),',',num2str(centers(1,2)),']']);
+     %drawnow();
+     %frame = frame+1;
+     %e = imellipse(gca, [sx sy 96 89]);
+     %flag=1;
+end
+ 
+figure(1);
+plot(ballpos(:,1), ballpos(:,2),'.-',...
+    ballpos(1,1), ballpos(1,2),'go', ballpos(end,1), ballpos(end,2),'rx');
+legend('Path','start','end');
+xlabel('x, (px)');
+ylabel('y, (px)');
+
--- Code/MainLoop.m
+++ Code/MainLoop.m
@@ -4,16 +4,63 @@ format compact
 close all
 clear all
 clc
-
+%SETUP for loops
+t1 = 0; %begin time for ODE solver
+t2  = 0.100; %end time for ODE solver
+minPulse = 1000e-6; %Min pulse width for hardware controller, Standard RC Mode
+maxPulse = 2000e-6; %Max pulse width for hardware controller, Standard RC Mode
 %limit = 0.8;
 %s = 0;
+obj = imaq.VideoDevice('dcam',1,'F7_Y8_640x480_mode0'); %camera link and object 
+display('Connecting to Arduino');
+a=arduino('/dev/ttyS101','Mega2560'); %create object for controller
+%controller; not needed for dummy testing
+s = servo(a, 'D4', 'MinPulseDuration', minPulse, 'MaxPulseDuration', maxPulse); %connect PWN control wire to digital pin #4.
+xyCMD = [400,400]; %Desired poistion for sphere. Can be an array of points but no support built in yet
+I_CMD_x = 0;
+I_CMD_y = 0;
+error_xy = [0,0];
+parameters();   %Load model parameters
+I=step(obj);    %Create link between I (image file) and camera object (RAW image stream) 
 
+%MAIN Loops
 while 1
     tmp = rand;
     if tmp > limit %Break loop condition
         break
     end
     %s = s + tmp;
+    %estimator run first and call the DummyArduino or RealArduino
+    %functions, Controller function required as well
+    function dx = motion(t,x)
+    dx = zeros(3,1);    % a column vector for equation set
+    dx(1) = x(2) * x(3);
+    dx(2) = -x(1) * x(3);
+    dx(3) = -0.51 * x(1) * x(2);
+    options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5]);
+    [~,X] = ode45(@motion,[t1 t2],[(centers(1,1)) 1 1],options);
+        
+    function dy = motion1(t,y)
+    dy = zeros(3,1);    % a column vector for equation set
+    dy(1) = y(2) * y(3);
+    dy(2) = -y(1) * y(3);
+    dy(3) = -0.51 * y(1) * y(2);
+    options = odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-5]);
+    [T,Y] = ode45(@motion,[t1 t2],[(centers(1,2)) 1 1],options);
+    %insert Contoller here
+    
+    %insert DummyArduino here
+    %RealArduino(I_CMD_x,I_CMD_y,s);
+    DummyArduino( I_CMD_x,I_CMD_y)
+    %insert image capture here
+    if (timer expired)
+        ImageTrack(centers);
+        reset timer
+    end
+    
+    x_pos_inital = centers(1,1);    %scale pixels to cm? May not be needed since the pixels may be good 
+    y_pos+inital = centers(1,2);    %scale pixels t cm? May not be needed since the pixels may be good
+    
     
     
 end
